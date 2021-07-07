7e2799a2737a259c3964975a717b91670aef207a
Vitaly Chernov <chernovitaly@gmail.com>
2021-07-07 12:54:15 +0400

    5_7_spring_data_jpa

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -6,9 +6,10 @@ public class Profiles {
 
     public static final String
             JDBC = "jdbc",
-            JPA = "jpa";
+            JPA = "jpa",
+            DATAJPA = "datajpa";
 
-    public static final String REPOSITORY_IMPLEMENTATION = JPA;
+    public static final String REPOSITORY_IMPLEMENTATION = DATAJPA;
 
     public static final String
             POSTGRES_DB = "postgres",
